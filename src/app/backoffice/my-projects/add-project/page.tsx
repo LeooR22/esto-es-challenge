f47003cda0f607773637fb3325317f343ee527a4
Leonel Rivas <leonelrivas.rl@gmail.com>
2024-07-01 13:49:16 -0300

    added routes and shadcn setup

---
+++ src/app/backoffice/my-projects/add-project/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-[calc(100vh-60px)] w-full flex-col items-center justify-center p-24">
+      <p>Add project</p>
+    </div>
+  );
+}
---
+++ src/app/backoffice/my-projects/edit-project/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-[calc(100vh-60px)] w-full flex-col items-center justify-center p-24">
+      <p>edit project</p>
+    </div>
+  );
+}
