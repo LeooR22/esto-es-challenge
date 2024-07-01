97b601445d242ad587dee107baec7379411272c7
Leonel Rivas <leonelrivas.rl@gmail.com>
2024-07-01 20:53:54 -0300

    added theme switcher

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import ThemeSwitch from "@/components/theme/ThemeSwitch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="bg-gray-800 text-white p-4 md:px-10 h-[60px]">
-          <h1 className="text-xl md: text-2xl font-bold">LOGO</h1>
-        </div>
-        {children}
+        <Providers>
+          <div className="bg-gray-800 text-white p-4 md:px-10 h-[60px]">
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl md: text-2xl font-bold">LOGO</h1>
+              <ThemeSwitch />
+            </div>
+          </div>
+          {children}
+        </Providers>
       </body>
     </html>
   );
