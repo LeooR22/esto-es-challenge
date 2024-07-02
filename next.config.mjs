53acce22c8e8e9fedba3e69399bc5b5a7954a74f
Leonel Rivas <leonelrivas.rl@gmail.com>
2024-07-02 09:53:52 -0300

    added logo in the header

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ],
+    unoptimized: true,
+  },
   redirects: async () => {
     return [
       {
         source: "/",
+        destination: "/backoffice/my-projects",
+        permanent: true,
+      },
+      {
+        source: "/backoffice",
         destination: "/backoffice/my-projects",
         permanent: true,
       },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import ThemeSwitch from "@/components/theme/ThemeSwitch";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +24,16 @@ export default function RootLayout({
         <Providers>
           <div className="bg-gray-800 text-white p-4 md:px-10 h-[60px]">
             <div className="flex items-center justify-between">
-              <h1 className="text-xl md: text-2xl font-bold">LOGO</h1>
+              {/* <h1 className="text-xl md: text-2xl font-bold">LOGO</h1> */}
+              <Link href={"/backoffice/my-projects"}>
+                <Image
+                  src="https://bitbucket.org/estoes-challenges/frontend/raw/1ec5e5f965bca436dda3a42dc2d186c500bda9b7/assets/images/logo.png"
+                  alt="Logo"
+                  width={60}
+                  height={60}
+                />
+              </Link>
+
               <ThemeSwitch />
             </div>
           </div>
